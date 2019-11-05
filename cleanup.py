da6f2a4dcff040099897777ed30eff61823f4873
YuQin <fgh0809@gmail.com>
2019-11-04 22:47:02 -0500

    update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -1,1 +1,70 @@
+import pandas as pd
+import numpy as np
+import sys
 
+OUTPUT = "real_data.csv"
+NAN = np.NaN
+
+def _is_number(x):
+    """
+    Return true if input unknown data type can be converted to a number (int or float).
+    """
+    try:
+        float(x)
+    except:
+        return False
+    else:
+        return True
+
+
+def _replace_non_alphanumeric(column):
+    """
+    Replace cells that contains invalid data to NA for a given column.
+    Data is invalid if a it is not a number and not alphabatical.
+    """
+    for i in range(len(column)):
+        if isinstance(column[i], str) and not _is_number(column[i]) and not column[i].isalpha():
+            # TODO replace data instead of messing up with copies.
+            column[i] = NAN
+    return column
+
+
+def read_dataframe(df_csv):
+    """
+    Read csv file into pandas dataframe, and fix all unicode.
+    """
+    with open(df_csv, 'r') as csvfile:
+        df = pd.read_csv(csvfile, sep=',' , encoding = 'utf-8')
+
+    # fix unicode
+    df.columns = [x.encode('utf-8').decode('ascii', 'ignore') for x in df.columns]
+    df = df.rename(columns = {'ommunityname':'communityname'})
+
+    return df
+
+
+def replace_invalid_cells(df):
+    """
+    Process all data and replace invalid data with NA.
+    """
+    for column in df:
+        df[column].replace(_replace_non_alphanumeric(df[column]))
+    return df
+
+
+def df_to_csv(df):
+    """
+    Dump dataframe to csv file.
+    """
+    df.to_csv(OUTPUT, encoding='utf-8', index=False)
+
+
+def main():
+    data = sys.argv[1]
+    df = read_dataframe(data)
+    df = replace_invalid_cells(df)
+    df_to_csv(df)
+
+
+if __name__ == "__main__":
+    main()
